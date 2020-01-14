db1181bb4e4534256db2d64f0f696edcf6ee02f4
MadisonViaLux <madison.s.mckown@gmail.com>
2020-01-13 17:30:02 -0700

    writing str()'s and int()'s

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,15 +11,20 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 print (sys.argv)
-
 for m in sys.argv:
     print 'argument: ', m
+#https://www.pythonforbeginners.com/argv/more-fun-with-sys-argv
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-print
+import platform
+print (platform.system() + platform.release(), "Version " + platform.version())
+#https://stackoverflow.com/questions/1854/what-os-am-i-running-on
+
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(sys.version)
+#https://stackoverflow.com/questions/1093322/how-do-i-check-what-version-of-python-is-running-my-script
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
