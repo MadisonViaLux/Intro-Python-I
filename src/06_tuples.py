6c818d252038050355a4bb4166cce9211bc2136e
MadisonViaLux <madison.s.mckown@gmail.com>
2020-01-14 22:27:40 -0700

    tuples are nonsenss

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -2,7 +2,7 @@
 Python tuples are sort of like lists, except they're immutable and
 are usually used to hold heterogenous data, as opposed to lists
 which are typically used to hold homogenous data. Tuples use
-parens instead of square brackets.
+parents instead of square brackets.
 
 More specifically, tuples are faster than lists. If you're looking
 to just define a constant set of values and that set of values
@@ -35,10 +35,18 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
-
+# Prints 1 2 5 7 99, one per line
 t = (1, 2, 5, 7, 99)
-print_tuple(t)  # Prints 1 2 5 7 99, one per line
+
+def print_tuple(z):
+    for i in z:
+        print(i)
+
+print_tuple(t)
+
+
+
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
